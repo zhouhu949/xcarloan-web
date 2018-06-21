a39d7f8f56d235975227c133782730d47d9a0c17
liminjian <1240023690@qq.com>
2018-06-21 10:23:29 +0800

    merge

--- environment/dev.env.js
+++ environment/dev.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   MOCK: false,
-  // URL_SERVER: JSON.stringify("http://192.168.2.2:8762/")
   URL_SERVER: JSON.stringify("http://192.168.3.71:8762/")
+  // URL_SERVER: JSON.stringify("http://192.168.3.198:8762/")
 }
